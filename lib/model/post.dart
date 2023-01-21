876ae1300a58220fdb706eee6da113a4dcfaee0c
kennedyowusu <kennediowusu@gmail.com>
2023-01-21 21:39:01 +0000

    add post model

---
+++ lib/model/post.dart
@@ -0,0 +1,97 @@
+// To parse this JSON data, do
+//
+// final post = postFromJson(jsonString);
+
+import 'dart:convert';
+
+Post postFromJson(String str) => Post.fromJson(json.decode(str));
+
+String postToJson(Post data) => json.encode(data.toJson());
+
+class Post {
+  Post({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.userId,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.liked,
+    required this.likesCount,
+    required this.user,
+  });
+
+  int? id = 0;
+  String? title = "";
+  String? description = "";
+  int? userId = 0;
+  DateTime? createdAt = DateTime.now();
+  DateTime? updatedAt = DateTime.now();
+  bool? liked = false;
+  int? likesCount = 0;
+  User? user;
+
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+    id: json["id"],
+    title: json["title"],
+    description: json["description"],
+    userId: json["user_id"],
+    createdAt: DateTime.parse(json["created_at"]),
+    updatedAt: DateTime.parse(json["updated_at"]),
+    liked: json["liked"],
+    likesCount: json["likes_count"],
+    user: User.fromJson(json["user"]),
+  );
+
+Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "description": description,
+    "user_id": userId,
+    "created_at": createdAt?.toIso8601String(),
+    "updated_at": updatedAt!.toIso8601String(),
+    "liked": liked,
+    "likes_count": likesCount,
+    "user": user!.toJson(),
+  };
+}
+
+class User {
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.emailVerifiedAt,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  int? id = 0;
+  String? name = "";
+  String? username = "";
+  String? email = "";
+  DateTime? emailVerifiedAt = DateTime.now();
+  DateTime? createdAt = DateTime.now();
+  DateTime? updatedAt = DateTime.now();
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    id: json["id"],
+    name: json["name"],
+    username: json["username"],
+    email: json["email"],
+    emailVerifiedAt: DateTime.parse(json["email_verified_at"]),
+    createdAt: DateTime.parse(json["created_at"]),
+    updatedAt: DateTime.parse(json["updated_at"]),
+  );
+
+Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "username": username,
+    "email": email,
+    "email_verified_at": emailVerifiedAt?.toIso8601String(),
+    "created_at": createdAt?.toIso8601String(),
+    "updated_at": updatedAt?.toIso8601String(),
+  };
+}
